91c4dd02e7b650bd1400c06f8c7630bb29d5596c
Joyce.Yeung <Joyce.Yeung@myob.com>
2020-08-20 20:32:20 +1000

    Created block class and added functions to generate a list of blocks

---
+++ Kata3/Block.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Kata3
+{
+    public class Block
+    {
+        public char FirstChar { get; set; }
+        public char SecondChar { get; set; }
+
+        public Block(char a,char b)
+        {
+            FirstChar = a;
+            SecondChar = b;
+
+        }
+    }
+}
---
+++ Kata3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Kata3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Something capable of holding two values of the block X
+//Create blocks
+//Container to hold all blocks
+//Focus on one char at each time
+//Find all blocks contains that one specific char
+//Check index number to ensure block has not been used
+//Decide which block to be used
+//Store which block has been used
+
